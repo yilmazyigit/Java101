586febd26713b1c82b6098e4451a65df5910acf7
Yiğit <83499976+yilmazyigit@users.noreply.github.com>
2022-06-18 21:11:32 +0300

    Update Main.java

--- pratik2/Main.java
+++ pratik2/Main.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double tutar, kdvOran = 0.18;
+        double tutar;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Tutari giriniz: ");
         tutar = input.nextDouble();
 
-        double kdvtutari = tutar * kdvOran;
-        double kdvliTutar = tutar + kdvtutari;
-
-        System.out.println(kdvliTutar);
+        if (tutar < 1000 && tutar >0) {
+            double kdvOran = 0.18;
+            double kdvtutari = tutar * kdvOran;
+            double kdvliTutar = tutar + kdvtutari;
+            System.out.println(kdvliTutar);
+        } else if (tutar > 1000) {
+            double kdvOran = 0.08;
+            double kdvtutari = tutar * kdvOran;
+            double kdvliTutar = tutar + kdvtutari;
+            System.out.println(kdvliTutar);
+        } else {
+            System.out.println("İslemleri kontrol edin..");
+        }
     }
 }
