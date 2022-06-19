2392c8887bacc6603a9f123af5ac2871def31bcc
Yiğit <83499976+yilmazyigit@users.noreply.github.com>
2022-06-20 00:47:34 +0300

    Add files via upload

---
+++ pratik23/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Lutfen n Degerinizi Giriniz :");
+        int n=input.nextInt();
+        double result=0;
+        for(double i=1.0;i<=n;i++){
+            result+=(1/i);
+        }
+        System.out.print(result+" Harmonik Sayisi");
+
+    }
+
+}
