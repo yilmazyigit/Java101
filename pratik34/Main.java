7a7ef260bc971dc0be5d10afd98e6d866a60ffef
Yiğit <83499976+yilmazyigit@users.noreply.github.com>
2022-06-20 23:27:16 +0300

    Add files via upload

---
+++ pratik34/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    static int result = 1;
+    static int us(int x, int y){
+        if (x == 0){
+            return 1;
+        }
+        result *= y;
+        us(x-1,y);
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("Tabandaki sayiyi giriniz: ");
+        y = input.nextInt();
+        System.out.println("Usteki sayiyi giriniz: ");
+        x = input.nextInt();
+        System.out.println(us(x,y));
+    }
+}
