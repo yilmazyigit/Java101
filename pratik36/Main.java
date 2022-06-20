2b7ac10c995b7fd6292b07e7203fefc12735c776
Yiğit <83499976+yilmazyigit@users.noreply.github.com>
2022-06-20 23:42:15 +0300

    Add files via upload

---
+++ pratik36/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    static int negative(int x) {
+        if (x <= 0){
+            return x;
+        }
+        else {
+            System.out.print(x + " ");
+            return negative(x-5);
+        }
+    }
+    static int positive(int x,int y) {
+        if(x > y){
+            return x;
+        }
+        else {
+            System.out.print(x + " ");
+            return positive(x+5,y);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (true){
+            System.out.println("Sayiyi giriniz : ");
+            int number = input.nextInt();
+
+            positive(negative(number),number);
+
+            if (number == 0){
+                break;
+            }
+            System.out.println("\nCikmak icin 0'i giriniz.");
+        }
+    }
+}
